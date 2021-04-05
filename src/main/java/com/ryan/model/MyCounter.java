c055aaeb10f6b1c6595782d2e83c6ebd69b30779
voduyquocthai <voduyquocthai@gmail.com>
2021-04-05 14:33:27 +0700

    added

---
+++ src/main/java/com/ryan/model/MyCounter.java
@@ -0,0 +1,25 @@
+package com.ryan.model;
+
+public class MyCounter {
+
+    private int count;
+
+    public MyCounter(int count) {
+        this.count = count;
+    }
+
+    public MyCounter() {
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int increment() {
+        return count++;
+    }
+}
